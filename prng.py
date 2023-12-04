b7c180e09c5ce73be8da410d23e561208f230842
Ian Hale <133537927+Halei-6103@users.noreply.github.com>
2023-12-04 00:06:11 -0800

    Update prng.py

--- prng.py
+++ prng.py
@@ -6,7 +6,7 @@ while True:
     with open("prng-service.txt", "r+") as file:
         content = file.read()
         if content.strip() == "run":
-            random_number = random.randint(0, 100)  # Adjust range as needed
+            random_number = random.randint(0, 100)  
             file.seek(0)
             file.write(str(random_number))
             file.truncate()
