f79a4a813c4cfd2e69a1b9e0a454c47a5eafc27f
Ian Hale <133537927+Halei-6103@users.noreply.github.com>
2023-12-04 00:01:08 -0800

    prng.py

---
+++ prng.py
@@ -0,0 +1,12 @@
+import time
+import random
+
+while True:
+    time.sleep(1)
+    with open("prng-service.txt", "r+") as file:
+        content = file.read()
+        if content.strip() == "run":
+            random_number = random.randint(0, 100)  # Adjust range as needed
+            file.seek(0)
+            file.write(str(random_number))
+            file.truncate()
